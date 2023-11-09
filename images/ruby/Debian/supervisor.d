9741071ff18e47114eccbbe1a9111e6425b26282
gtarique <gazi.tarique@intelletive.com>
2023-11-09 16:42:04 +0600

    Add non root user.

--- images/ruby/Debian/supervisor.d
+++ images/ruby/Debian/supervisor.d
@@ -1,12 +1,11 @@
 [supervisord]
 nodaemon=true
 logfile=/var/log/supervisor/supervisord.log
-pidfile=/var/run/supervisord.pid
-user=root
 
 [program:rails]
 command=/bin/bash -c "./scripts/start_rails.sh"
 autostart=true
 autorestart=true
+user=appuser
 stdout_logfile=/var/log/supervisor/rails_stdout.log
 stderr_logfile=/var/log/supervisor/rails_stderr.log
