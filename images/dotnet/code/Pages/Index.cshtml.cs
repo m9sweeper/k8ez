d66b87942f96331dd01d7af695fefcba65724240
gtarique <gazi.tarique@intelletive.com>
2023-12-18 18:00:06 +0600

    Log message added.

--- images/dotnet/code/Pages/Index.cshtml.cs
+++ images/dotnet/code/Pages/Index.cshtml.cs
@@ -10,6 +10,7 @@ public class IndexModel : PageModel
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        Console.WriteLine("Hello world!!");
     }
 
     public void OnGet()
